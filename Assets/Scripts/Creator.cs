32d0a4806d1084d85d5bd2402c43a661f91ef43b
0Alic <andrealisi.12lj@gmail.com>
2018-04-28 16:39:21 +0200

    Added Signor meshes

---
+++ Assets/Scripts/Creator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Creator : MonoBehaviour {
+
+	public GameObject[] prefabs;
+
+	void Start()
+	{
+
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnGUI(){
+	
+	}
+
+	public void addObject(){
+	
+		Instantiate(prefabs[0], new Vector3(0, 0, 0), Quaternion.identity);
+	}
+}
