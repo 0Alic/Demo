79da75fff0d3291a2fef9bf7eab6264b12953544
0Alic <andrealisi.12lj@gmail.com>
2018-05-01 12:29:12 +0200

    Added Twity Plugin

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class InteractableObject : MonoBehaviour {
 
 
 	// Use this for initialization
 	void Start () {  
 		
+
 	}
 	
 	// Update is called once per frame
