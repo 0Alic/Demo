3614d420a6d1ca7ba46076b68840613ce0995420
giulioaur <giulioau@gmail.com>
2018-05-01 11:50:58 +0200

    Added serialization.
    Andrea sempre culo.

---
+++ Assets/Scripts/DictonaryEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DictonaryEntity : MonoBehaviour {
+
+	static PrefabDictonary dic = PrefabDictonary.Instance;
+	int id;
+
+	// Use this for initialization
+	public void AddEntity (string prefabName, Vector3 position, Quaternion rotation) {
+		id = dic.AddEntity(prefabName, position, rotation);
+	}
+	
+	// Update is called once per frame
+	public void UpdatePosition (Vector3 position) {
+		dic.UpdatePosition(id, position);
+	}
+
+	public int ID{
+		get{
+			return id;
+		}
+		set{
+			this.id = value;
+		}
+	}
+}
--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -6,7 +6,7 @@ public class InteractableObject : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
+	void Start () {  
 		
 	}
 	
