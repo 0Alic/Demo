567ef5f3a8d0ee9f9632d2e932a925ca7404c834
giulioaur <giulioau@gmail.com>
2018-05-05 19:42:27 +0200

    Added tweet visualization

--- Assets/Scripts/GetTwitter.cs
+++ Assets/Scripts/GetTwitter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
 using Twity.DataModels.Responses;
 
 public class GetTwitter : MonoBehaviour {
@@ -14,7 +16,7 @@ public class GetTwitter : MonoBehaviour {
 		Twity.Oauth.accessTokenSecret= TwitterKeys.AccessTokenSecret;
 
 		Dictionary<string, string> parameters = new Dictionary<string, string>();
-		parameters ["count"] = 30.ToString();;
+		parameters ["count"] = 2.ToString();
 		StartCoroutine (Twity.Client.Get ("statuses/home_timeline", parameters, Callback));
 	}
 		
@@ -22,8 +24,23 @@ public class GetTwitter : MonoBehaviour {
 		
 		if (success) {
 			StatusesHomeTimelineResponse Response = JsonUtility.FromJson<StatusesHomeTimelineResponse> (response);
-			
-			Debug.Log(Response.items[0].text);
+
+			// Retrieve text box.
+			TMPro.TextMeshPro author1 = transform.Find("Tweet1_author").gameObject.GetComponent<TMPro.TextMeshPro>(), 
+							tweet1 = transform.Find("Tweet1").gameObject.GetComponent<TMPro.TextMeshPro>(),
+							author2 = transform.Find("Tweet2_author").gameObject.GetComponent<TMPro.TextMeshPro>(), 
+							tweet2 = transform.Find("Tweet2").gameObject.GetComponent<TMPro.TextMeshPro>();;
+
+			// Print the tweets and their author.
+			if(author1 != null && tweet1 != null){
+				author1.text = Response.items[0].user.name;
+				tweet1.text = Response.items[0].text;
+			}
+
+			if(author2 != null && tweet2 != null){
+				author2.text = Response.items[1].user.name;
+				tweet2.text = Response.items[1].text;
+			}
 		} else {
 			Debug.Log (response);
 		}
