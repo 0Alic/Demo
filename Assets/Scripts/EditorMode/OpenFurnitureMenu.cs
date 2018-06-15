c24995a5632ce43b9ef88a053ebe63509c034d78
0Alic <andrealisi.12lj@gmail.com>
2018-06-15 16:55:52 +0200

    added arm menu

--- Assets/Scripts/EditorMode/OpenFurnitureMenu.cs
+++ Assets/Scripts/EditorMode/OpenFurnitureMenu.cs
@@ -5,31 +5,63 @@ using UnityEngine.UI;
 
 public class OpenFurnitureMenu : MonoBehaviour {
 
+	public Canvas canvas;
+	public Transform lookTarget;
+	private Transform hand;
+	
+	private List<Vector3> buttons;
 	private bool start = false;
-	List<Vector3> buttons;
 
+
+	public float height = 0.3f;
 	void Start () {
 		
+		hand = GameObject.Find("Mano").transform;
+		transform.position = hand.position + hand.forward*7;
+
 		buttons = new List<Vector3>();
 
-		for(int i=0; i<transform.childCount; i++){
-			buttons.Add(transform.GetChild(i).localPosition);
-			Debug.Log("i " + transform.GetChild(i).localPosition);
+		Transform child = transform.GetChild(0); // The child is the canvas
+		for(int i=0; i<child.childCount; i++){
+			buttons.Add(child.GetChild(i).localPosition);
 		}		
 	}
 	
 	void Update () {
 		
-		// TODO sarebbe carino metterlo in una coroutine che si avvia solo quando chiami questo menu
-		for(int i=0; i<buttons.Count; i++){
+		transform.LookAt(lookTarget);
+		transform.position = hand.position + Vector3.up*height;
 
-			transform.GetChild(i).localPosition = Vector3.Lerp(transform.GetChild(i).localPosition, buttons[i] + new Vector3(50*i, 0, 0), Time.deltaTime);
+		if(Input.GetKeyDown(KeyCode.Tab)){
+
+			if(!canvas.gameObject.activeInHierarchy) {
+
+				canvas.gameObject.SetActive(true);
+				StartCoroutine("SfogliaCoroutine");
+			}
+			else {				
+
+				canvas.gameObject.SetActive(false);
+				Transform child = transform.GetChild(0);				
+				for(int i=0; i<buttons.Count; i++)
+					child.GetChild(i).localPosition = new Vector3(-100, 0, 0);
+				StopCoroutine("SfogliaCoroutine");
+			}
 		}
 	}
 
-	IEnumerator sfogliaCoroutine() {
+	IEnumerator SfogliaCoroutine() {
 
+		Transform child = transform.GetChild(0);
+		// BRutta condizione di uscita
+		while(child.GetChild(buttons.Count-1).position != buttons[buttons.Count-1] + new Vector3(50*buttons.Count-1, 0, 0)) {
 
-		return null;
+			for(int i=0; i<buttons.Count; i++)
+				child.GetChild(i).localPosition = Vector3.Lerp(child.GetChild(i).localPosition, buttons[i] + new Vector3(50*i, 0, 0), Time.deltaTime);
+
+			yield return new WaitForEndOfFrame();
+		}
+		Debug.Log("UDIGFKUDSYGVFDSKYUGVFDSKGVFDSGVYDFVBDSJYVBSJF");
+		yield return null;
 	}
 }
